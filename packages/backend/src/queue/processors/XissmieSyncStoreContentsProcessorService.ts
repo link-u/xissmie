fcb845248736f6a6453d5c7c4bc9b27f0e1c38a1
syuilo <4439005+syuilo@users.noreply.github.com>
2025-10-04 10:05:19 +0900

    Create XissmieSyncStoreContentsProcessorService.ts

---
+++ packages/backend/src/queue/processors/XissmieSyncStoreContentsProcessorService.ts
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { setTimeout } from 'node:timers/promises';
+import { Inject, Injectable } from '@nestjs/common';
+import { DataSource, IsNull, LessThan, QueryFailedError, Not } from 'typeorm';
+import { DI } from '@/di-symbols.js';
+import type { MiMeta } from '@/models/_.js';
+import type Logger from '@/logger.js';
+import { bindThis } from '@/decorators.js';
+import { IdService } from '@/core/IdService.js';
+import { QueueLoggerService } from '../QueueLoggerService.js';
+import type * as Bull from 'bullmq';
+
+@Injectable()
+export class XissmieSyncStoreContentsProcessorService {
+	private logger: Logger;
+
+	constructor(
+		@Inject(DI.meta)
+		private meta: MiMeta,
+
+		@Inject(DI.db)
+		private db: DataSource,
+
+		private idService: IdService,
+		private queueLoggerService: QueueLoggerService,
+	) {
+		this.logger = this.queueLoggerService.logger.createSubLogger('xissmie-sync-store-contents');
+	}
+
+	@bindThis
+	public async process(job: Bull.Job<Record<string, unknown>>): Promise<void> {
+
+	}
+}
